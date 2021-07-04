6517f20215cc1c924a84f97d268264d43745c505
Carmelo Michaele <cmichael@ev-n6.kzn.21-school.ru>
2021-07-04 15:58:41 +0300

    little fix norm 3

--- ex00/main.c
+++ ex00/main.c
@@ -37,56 +37,72 @@ void	write_to_4array(int *arr, int a[4]);
 
 void	print4na4(int result[4][4]);
 
-int	find_result(int result[4][4], int bs[4][4])
+int	find_sub_sub_result(int v[4], int result[4][4], int bs[4][4])
 {
-	int	v1;
-	int	v2;
-	int	v3;
-	int	v4;
 	int	checkresult;
 
-	v1 = 0;
-	v2 = 0;
-	v3 = 0;
-	v4 = 0;
 	checkresult = 0;
-	while (checkresult == 0 && (v1 < 24 && v2 < 24 && v3 < 24 && v4 < 24))
+	while (v[2] < 24 && !checkresult)
 	{
-		while (v1 < 24 && !checkresult)
+		if (g_vs[v[2]][0] == bs[2][2] && g_vs[v[2]][1] == bs[3][2])
 		{
-			if (g_vs[v1][0] == bs[2][0] && g_vs[v1][1] == bs[3][0])
+			write_to_4array(result[2], g_vs[v[2]]);
+			v[3] = 0;
+			while (v[3] < 24 && !checkresult)
 			{
-				write_to_4array(result[0], g_vs[v1]);
-				v2 = 0;
-				while (v2 < 24 && !checkresult)
+				if (g_vs[v[3]][0] == bs[2][3] && g_vs[v[3]][1] == bs[3][3])
 				{
-					if (g_vs[v2][0] == bs[2][1] && g_vs[v2][1] == bs[3][1])
-					{
-						write_to_4array(result[1], g_vs[v2]);
-						v3 = 0;
-						while (v3 < 24 && !checkresult)
-						{
-							if (g_vs[v3][0] == bs[2][2] && g_vs[v3][1] == bs[3][2])
-							{
-								write_to_4array(result[2], g_vs[v3]);
-								v4 = 0;
-								while (v4 < 24 && !checkresult)
-								{
-									if (g_vs[v4][0] == bs[2][3] && g_vs[v4][1] == bs[3][3])
-									{
-										write_to_4array(result[3], g_vs[v4]);
-										checkresult = check_result(result, bs);
-									}
-									v4++;
-								}
-							}
-							v3++;
-						}
-					}
-					v2++;
+					write_to_4array(result[3], g_vs[v[3]]);
+					checkresult = check_result(result, bs);
 				}
+				v[3]++;
 			}
-			v1++;
+		}
+		v[2]++;
+	}
+	return (checkresult);
+}
+
+int	find_sub_result(int v[4], int result[4][4], int bs[4][4])
+{
+	int	checkresult;
+
+	checkresult = 0;
+	while (v[1] < 24 && !checkresult)
+	{
+		if (g_vs[v[1]][0] == bs[2][1] && g_vs[v[1]][1] == bs[3][1])
+		{
+			write_to_4array(result[1], g_vs[v[1]]);
+			v[2] = 0;
+			checkresult = find_sub_sub_result(v, result, bs);
+		}
+		v[1]++;
+	}
+	return (checkresult);
+}
+
+int	find_result(int result[4][4], int bs[4][4])
+{
+	int	v[4];
+	int	checkresult;
+
+	v[0] = 0;
+	v[1] = 0;
+	v[2] = 0;
+	v[3] = 0;
+	checkresult = 0;
+	while (checkresult == 0 && (v[0] < 24
+			&& v[1] < 24 && v[2] < 24 && v[3] < 24))
+	{
+		while (v[0] < 24 && !checkresult)
+		{
+			if (g_vs[v[0]][0] == bs[2][0] && g_vs[v[0]][1] == bs[3][0])
+			{
+				write_to_4array(result[0], g_vs[v[0]]);
+				v[1] = 0;
+				checkresult = find_sub_result(v, result, bs);
+			}
+			v[0]++;
 		}
 	}
 	return (checkresult);
