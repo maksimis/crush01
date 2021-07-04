6e50dca0aa916011e29929f1214b7c5a73fafe77
Carmelo Michaele <cmichael@ev-n6.kzn.21-school.ru>
2021-07-04 17:08:12 +0300

    комменты2

--- ex00/var.h
+++ ex00/var.h
@@ -1,5 +1,5 @@
 #ifndef VAR_H
 # define VAR_H
-extern int	g_vs[24][6];
+extern int	g_vs[24][6]; //этот файл для того чтобы расшарить глобальную переменную по двум файлам
 extern int	g_count;
 #endif //VAR_H
